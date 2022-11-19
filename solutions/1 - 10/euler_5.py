56c78f5bed1063508f78874c327d7df7424e8892
Jordan <jordanrodrigues@hotmail.co.uk>
2022-11-19 15:12:40 +0000

    Solved Euler 5

--- solutions/1 - 10/euler_5.py
+++ solutions/1 - 10/euler_5.py
@@ -1,3 +1,19 @@
-from .. import resources
+"""
+    EULER 5: 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 
-resources.test_import()
+    What is the smallest possible number evenly divisible by all numbers from 1 to 20?
+"""
+import time
+start_time = time.time()
+
+working_num = 2520
+i = 2
+
+while i <= 20:
+    if working_num % i !=0:
+        working_num += 20
+        i = 1
+    i += 1
+
+print("Smallest number divisible by all numbers from 1 to 20 is: {Answer}".format(Answer=working_num))
+print("--- %s seconds ---" % (time.time() - start_time))
