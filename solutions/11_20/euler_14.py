fee5d22847008f82c0cbb780e6e737d85e03a979
Jordan <jordanrodrigues@hotmail.co.uk>
2022-11-30 19:00:15 +0000

    Solved Euler 14

---
+++ solutions/11_20/euler_14.py
@@ -0,0 +1,22 @@
+#Collatz sequence
+longest_chain = []
+
+for i in range(2,1000000):
+    working_chain = [i]
+
+    n = i
+    while n != 1:
+        if n % 2 == 0:
+            n = int(n/2)
+            working_chain.append(n)
+
+        elif n % 2 != 0:
+            n = (3 * n) + 1
+            working_chain.append(n)
+    if len(working_chain) > len(longest_chain):
+        longest_chain = working_chain
+
+print(str(longest_chain[0]))
+
+
+
